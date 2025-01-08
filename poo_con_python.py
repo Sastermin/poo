394b5777ff7f094d67bb8da5ebe420c3e2733251
Sastermin <dar007alex@gmail.com>
2025-01-08 08:49:55 -0600

    Creando atributos de la clase personaje

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,1 +1,15 @@
-print("Hola")
+class Personaje:
+    #atributos de la clase
+    nombre = 'default'
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vida = 0
+
+#variable de constructor vacío de la clase
+mi_personaje = Personaje()
+#modificando los valores de los atributos
+mi_personaje.nombre = "Dom"
+mi_personaje.fuerza = 100
+print("El nombre del personaje es ",mi_personaje.nombre)
+print("La fuerza del personaje es ",mi_personaje.fuerza)

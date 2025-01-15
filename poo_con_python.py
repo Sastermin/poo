bb75931e48ae73fc4cb9c47e5582c21a4a086ad0
Sastermin <dar007alex@gmail.com>
2025-01-15 08:35:54 -0600

    Encapsulando atributos

--- poo_con_python.py
+++ poo_con_python.py
@@ -2,47 +2,66 @@ class Personaje:
     
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
         #El signo de "=" es de asignación
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa
-        self.vida = vida
+        self.__nombre = nombre
+        self.__fuerza = fuerza
+        self.__inteligencia = inteligencia
+        self.__defensa = defensa
+        self.__vida = vida
         
     def atributos(self):
-        print(self.nombre)
-        print("Fuerza: ", self.fuerza)
-        print("Inteligencia: ", self.inteligencia)
-        print("Defensa: ", self.defensa)
-        print("Vida: ", self.vida)
+        print(self.__nombre)
+        print("Fuerza: ", self.__fuerza)
+        print("Inteligencia: ", self.__inteligencia)
+        print("Defensa: ", self.__defensa)
+        print("Vida: ", self.__vida)
     
     def subir_nivel(self,fuerza, inteligencia, defensa):
-        self.fuerza += fuerza
-        self.inteligencia += inteligencia
-        self.defensa += defensa
+        self.__fuerza += fuerza
+        self.__inteligencia += inteligencia
+        self.__defensa += defensa
         
     def esta_vivo(self):
-        return self.vida > 0
+        return self.__vida > 0
     
     def morir(self):
-        self.vida = 0
-        print(self.nombre, "ha muerto")
+        self.__vida = 0
+        print(self.__nombre, "ha muerto")
         
     def dañar(self, enemigo):
-        return self.fuerza - enemigo.defensa
+        return self.__fuerza - enemigo.__defensa if self.__fuerza >= self.__defensa else 0
     
     def atacar(self, enemigo):
         daño = self.dañar(enemigo)
-        enemigo.vida = enemigo.vida - daño
-        print(self.nombre, "ha realizado", daño, "puntos de daño a", enemigo.nombre)
-        print("Vida de ", enemigo.nombre, "es", enemigo.vida)
+        enemigo.__vida = enemigo.__vida - daño
+        print(self.__nombre, "ha realizado", daño, "puntos de daño a", enemigo.__nombre) 
+        if not enemigo.esta_vivo():
+            enemigo.morir()
+        print("Vida de ", enemigo.__nombre, "es", enemigo.__vida)
+        
+    def get_fuerza(self): 
+        return self.__fuerza
+    
+    def set_fuerza(self, fuerza):
+        if fuerza < 0: 
+            print("ERROR, haz puesto un valor negativo")        
+        self.__fuerza = fuerza
+        
+    
     
         
-        
 #variable de constructor de la clase
-mi_personaje = Personaje("trakalosa de monterrey", 70, 90, 50, 100)
+mi_personaje = Personaje("Trakalosa de monterrey", 8000, 90, 50, 100)
 mi_enemigo = Personaje("La Arrolladora", 60, 90, 40, 100)
 #print(mi_personaje.dañar(mi_enemigo))
-mi_personaje.atributos()
-mi_personaje.atacar(mi_enemigo)
-mi_enemigo.atributos()
 #print(mi_personaje.esta_vivo())
+#mi_personaje.atributos()
+#mi_personaje.atacar(mi_enemigo)
+#mi_enemigo.atributos()
+
+#Prueba 4. ¿Acceso a morir?
+mi_personaje.atacar(mi_enemigo)
+
+#Prueba 7. Getters and setters
+print(mi_personaje.get_fuerza())
+mi_personaje.set_fuerza(-100)
+print(mi_personaje.get_fuerza())
